1ac8b93f9dc27effdece4ebd27e4b017146e193d
SedanurAkçil <66331295+sedanurakcil@users.noreply.github.com>
2023-12-12 20:20:52 +0300

    created events file

---
+++ src/utils/events.ts
@@ -0,0 +1,7 @@
+/**
+ * ! This file is used to specify error event types for crashltytics.
+ */
+export const INITIATE_TENANT_CONFIGS = "initiate_tenant_configs";
+export const GET_USERS_ONLINE_INFO = "get_users_online_info";
+export const GET_ROOMS_READ_TOKEN = "get_rooms_read_token";
+export const INITIALIZE_SOCKET_CONNECT = "initialize_socket_connect";
