949625985456da498d844e35b67ef0398bac9fd5
SedanurAkçil <66331295+sedanurakcil@users.noreply.github.com>
2024-02-06 14:14:35 +0300

    deleted Dark theme colors

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -2,16 +2,11 @@ import TYPOGRAPHY from "./typography";
 import SPACINGS from "./spacings";
 import COLORS from "./colors";
 
-const { DARK_THEME_COLORS, LIGHT_THEME_COLORS } = COLORS;
+const { LIGHT_THEME_COLORS } = COLORS;
 
 const COMMON = {
   typography: TYPOGRAPHY,
   spacing: SPACINGS,
-};
-
-export const DARK_THEME = {
-  ...COMMON,
-  colors: DARK_THEME_COLORS,
 };
 
 export const LIGHT_THEME = {
@@ -20,7 +15,7 @@ export const LIGHT_THEME = {
 };
 
 interface Theme {
-  colors: typeof DARK_THEME_COLORS | typeof LIGHT_THEME_COLORS;
+  colors: typeof LIGHT_THEME_COLORS;
   spacing: typeof SPACINGS;
   typography: typeof TYPOGRAPHY;
 }
